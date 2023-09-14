52e38abf8859fa21a3ba8512c3c79c3cfc872f1c
Muhamad Lutfi <lutfi787878@gmail.com>
2023-09-14 13:48:46 +0700

    commit js

--- controller.php
+++ controller.php
@@ -2,6 +2,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>js</title>
+</head>
+<body>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        class_exists
+    </body>
+    </html>
+</body>
+</html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=cod
+    , initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
